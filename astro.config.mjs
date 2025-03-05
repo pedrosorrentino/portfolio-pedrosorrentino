34b2bed9bf985833b281af19543f5f32c063fe13
pedrosorrentino <elpelusashop@gmail.com>
2025-03-05 16:34:42 +0700

    Punto de control

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   site: 'https://pedrosorrentino.com',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
